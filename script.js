d0488a84bbbfdbce92f39c17eb871389b657f1bd
Habeeb1213 <iyiadehabeeblah@gmail.com>
2025-08-25 21:03:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,18 @@
-let number1 = "";
-let number2 = "";
-let operator = "";
-let isSecondNumber = false;
+const result = document.getElementById("result");
 
-function getAnswer(value) {
-  if (value === "+" || value === "-" || value === "*" || value === "/") {
-    operator = value;
-    isSecondNumber = true;
-    document.getElementById("result").value = operator;
-  } else {
-    if (!isSecondNumber) {
-      number1 += value;
-      document.getElementById("result").value = number1;
-    } else {
-      number2 += value;
-      document.getElementById("result").value = number2;
-    }
-  }
+function getAnswer(input) {
+  result.value += input;
 }
 
-function clearAll() {
-  number1 = "";
-  number2 = "";
-  operator = "";
-  isSecondNumber = false;
-  document.getElementById("result").value = "";
+function clearAll(params) {
+  result.value = "";
 }
 
 function calculateResult() {
-  let num1 = parseFloat(number1);
-  let num2 = parseFloat(number2);
-  let result = 0;
-
-  if (operator === "+") {
-    result = num1 + num2;
-  } else if (operator === "-") {
-    result = num1 - num2;
-  } else if (operator === "*") {
-    result = num1 * num2;
-  } else if (operator === "/") {
-    if (num2 === 0) {
-      document.getElementById("result").value = "Error";
-      return;
-    }
-    result = num1 / num2;
-  } else {
-    document.getElementById("result").value = "Error";
-    return;
+  try {
+    result.value = eval(result.value)
+  } catch (error) {
+    result.value = "Can't Calculate"
   }
-
-  document.getElementById("result").value = result;
-
-  // Reset for next calculation
-  number1 = result.toString();
-  number2 = "";
-  operator = "";
-  isSecondNumber = false;
+  
 }
